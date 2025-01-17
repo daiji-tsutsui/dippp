ab32fa9a108a70ada75ec15638f522faf8b5754e
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2025-01-17 13:21:47 +0900

    Salutationのテスト追加

--- src/salutation.rs
+++ src/salutation.rs
@@ -14,3 +14,40 @@ impl<T: MessageWriter> Salutation<T> {
         self.writer.write(message);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_exclaim() {
+        let writer = StubWriter::new();
+        let mut salute = Salutation::new(writer);
+        salute.exclaim();
+        assert_eq!(salute.get_writer().written.len(), 1);
+        assert_eq!(salute.get_writer().written[0], "Hello, DI!");
+    }
+
+    #[derive(Clone)]
+    struct StubWriter {
+        pub written: Vec<String>,
+    }
+
+    impl MessageWriter for StubWriter {
+        fn write(&mut self, message: String) {
+            self.written.push(message);
+        }
+    }
+
+    impl StubWriter {
+        pub fn new() -> Self {
+            Self { written: Vec::<String>::new() }
+        }
+    }
+
+    impl<T: MessageWriter + Clone> Salutation<T> {
+        pub fn get_writer(&self) -> T {
+            self.writer.clone()
+        }
+    }
+}
