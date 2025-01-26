ddda3f95799595bf6a0a66977212d944185bdabf
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2025-01-26 22:24:36 +0900

    UT追加

--- chap3/src/view_model/product_view_model.rs
+++ chap3/src/view_model/product_view_model.rs
@@ -10,3 +10,20 @@ impl ProductViewModel {
         format!("{} (${:.2})", self.data.name, self.data.unit_price)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_summary_text() {
+        let view_model = ProductViewModel {
+            data: product::Product {
+                name: String::from("test"),
+                unit_price: 200.0,
+                ..Default::default()
+            },
+        };
+        assert_eq!(view_model.summary_text(), "test ($200.00)");
+    }
+}
