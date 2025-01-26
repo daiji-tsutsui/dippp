7cb4f9617f95c63cb205c384a23f0f001d9c0cf9
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2025-01-27 00:34:57 +0900

    ドメインロジックに必要なカラムだけで良いらしい

--- chap3/src/model/product.rs
+++ chap3/src/model/product.rs
@@ -2,10 +2,9 @@ use super::*;
 
 #[allow(dead_code)]
 #[derive(Clone, Default, Debug)]
+// Only necessary columns for our domain logic
 pub struct Product {
-    pub id: i32,
     pub name: String,
-    pub desc: String,
     pub unit_price: f32,
     pub is_featured: bool,
 }
