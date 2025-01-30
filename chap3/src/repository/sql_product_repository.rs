ac759f4598c1f61295940134659c7e9b710abcf8
Daiji Tsutsui <daiji.tsutsui417@gmail.com>
2025-01-31 01:15:03 +0900

    SqlProductRepositoryのget_featured_products実装

--- chap3/src/repository/sql_product_repository.rs
+++ chap3/src/repository/sql_product_repository.rs
@@ -1,15 +1,19 @@
-// use crate::db_context::DbContext;
+use crate::db_context::DbContext;
 use crate::model::product;
 use super::i_product_repository::IProductRepository;
 
 pub struct SqlProductRepository {
-    // context: DbContext,
+    context: DbContext,
+}
+
+impl SqlProductRepository {
+    pub fn new(context: DbContext) -> Self {
+        Self { context }
+    }
 }
 
 impl IProductRepository for SqlProductRepository {
     fn get_featured_products(&self) -> Vec<product::Product> {
-        vec![
-            product::Product::new(),
-        ]
+        self.context.fetch_products("is_featured", "true")
     }
 }
